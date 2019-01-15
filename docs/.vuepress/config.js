58d17369e76d4fbca9a57b0e5033bfaf730a68ae
evinLiang <evin@evinweb.com>
2019-01-15 17:19:08 +0800

    第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '技术文档',
+  description: '自己收集的',
+  themeConfig: {
+    // 添加导航栏
+    nav: [
+      { text: '首页', link: '/' },
+			{ text: 'HTML', link: '/HTML/' },
+      { text: 'CSS', link: '/CSS/' },
+      { text: 'JAVASCRIPT', link: '/JAVASCRIPT/' },
+    ]
+  },
+}
